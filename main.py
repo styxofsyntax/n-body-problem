dbb27b04881d77f77d88fcea0e972bb5144242b8
codingInEME <107263899+codingInEME@users.noreply.github.com>
2024-06-23 20:37:29 +0500

    Refactor Body and App Class
    - Converted constructor arguments into properties for improved
    readability and configuration.
    - Implemented trails for bodies to visualize their paths.
    - Added color properties for bodies, background and trail.
    - Added property to append custom bodies with randomly generated ones.

--- main.py
+++ main.py
@@ -6,12 +6,24 @@ G = 0.01
 
 
 class Body:
-    def __init__(self, mass, pos, velocity, color='medium sea green'):
+    def __init__(self, mass, pos, velocity, log_size, color=None, trail_color=None):
+
         self.mass = mass
         self.pos = np.array(pos, dtype=float)
         self.velocity = np.array(velocity, dtype=float)
         self.radius = (self.mass * np.pi * 0.75) ** (1./3)
-        self.color = color
+
+        if color is None:
+            self.color = "medium sea green"
+        else:
+            self.color = color
+
+        if trail_color is None:
+            self.trail_color = "darkslategray"
+        else:
+            self.trail_color = trail_color
+
+        self.log_pos = [*pos] + [None for _ in range(log_size - 2)]
 
     def distance(self, body):
         return np.sqrt(np.sum((self.pos - body.pos) ** 2))
@@ -20,35 +32,48 @@ class Body:
         return (G * self.mass * body.mass) / (self.distance(body) ** 2)
 
     @staticmethod
-    def generate_bodies(n, mass_max, x_max, y_max, velocity_max):
+    def generate_bodies(properties):
         bodies = []
 
-        for _ in range(n):
-            mass = random.random() * mass_max
-            x = random.uniform(10, x_max)
-            y = random.uniform(10, y_max)
+        for _ in range(properties['num_bodies']):
+            mass = random.random() * properties['mass_max']
+            x = random.uniform(10, properties['x_max'])
+            y = random.uniform(10, properties['y_max'])
             velocity = np.array(
-                [random.random() * velocity_max, random.random() * velocity_max], dtype=float)
+                [random.random() * properties['velocity_max'], random.random() * properties['velocity_max']], dtype=float)
 
-            bodies.append(Body(mass, [x, y], velocity))
+            if properties['random_color']:
+                color = "#"+("%06x" % random.randint(0, 16777215))
+            else:
+                color = properties['color']
+
+            bodies.append(
+                Body(mass, [x, y], velocity, properties['trail_length'], color, properties['trail_color']))
 
         return bodies
 
 
 class App(tk.Tk):
-    def __init__(self, width, height, bodies):
+    def __init__(self, properties):
         root = super().__init__()
 
-        self.width = width
-        self.height = height
-        self.bodies = bodies
+        self.width = properties['width']
+        self.height = properties['height']
+
+        if properties['generate_bodies']:
+            self.bodies = Body.generate_bodies(properties['body'])
+            if properties['append_bodies']:
+                self.bodies += (properties['bodies'])
+        else:
+            self.bodies = properties['bodies']
+
         self.universe = tk.Canvas(
-            root, width=self.width, height=self.height, bg='gray9')
+            root, width=self.width, height=self.height, bg=properties['bg_color'])
         self.universe.pack()
 
-        self.render_universe()
+        self.render_universe(properties['max_time'], properties['start_time'])
 
-    def render_universe(self, max_t=10000, t=0):
+    def render_universe(self, max_t, t):
         tx = 20
         dt = 80
         t += 1
@@ -76,13 +101,6 @@ class App(tk.Tk):
                 acc = force / body.mass
                 velocity = acc * dt
 
-                # print(f'distance: {body.distance(body_other)}')
-                # print(f'force: {force}')
-                # print(f'acceleration: {acc}')
-                # print(f'angle: {angle}')
-                # print(f'velocity: {velocity}')
-                # print(f'body velocity: {body.velocity}')
-
                 if dx > 0:
                     body.velocity[0] += velocity * np.cos(angle)
                 else:
@@ -105,6 +123,10 @@ class App(tk.Tk):
             body_border = np.concatenate(
                 (body.pos - body.radius, body.pos + body.radius))
 
+            body.log_pos = [*body.pos] + body.log_pos[:-2]
+
+            self.universe.create_line(
+                body.log_pos, smooth=True, width=2, fill=body.trail_color)
             self.universe.create_oval(
                 *body_border, fill=body.color, outline=body.color)
 
@@ -112,20 +134,35 @@ class App(tk.Tk):
 
 
 bodies = [
-    # Body(10, 200, 200, [0.1, -0.1]),
-    # Body(10, 400, 400, [-0.1, 0.1]),
-    Body(10, [450, 250], [4, 0], 'MediumPurple2'),
-    Body(10, [350, 250], [4, 0], 'firebrick3'),
-    Body(10, [350, 150], [1, 0], 'DeepSkyBlue2')
+    Body(10, [350, 250], [3.5, 0], 80, 'firebrick3'),
+    Body(10, [350, 150], [2.5, 0], 80, 'DeepSkyBlue2'),
+    Body(10, [350, 50], [2, 0], 80, 'MediumPurple2'),
+    Body(1500, [350, 350], [0, 0], 80)
 ]
-# print(bodies[0].distance(bodies[1]))
-# print(bodies[0].gravity_force(bodies[1]))
 
 X_SIZE, Y_SIZE = (700, 700)
 
-# bodies = Body.generate_bodies(
-#     20, 100, X_SIZE - 50, Y_SIZE - 50, 0)
-bodies.append(Body(1500, [350, 350], [0, 0]))
+properties = {
+    'width': X_SIZE,
+    'height': Y_SIZE,
+    'bg_color': 'gray9',
+    'max_time': 10000,
+    'start_time': 0,
+    'generate_bodies': False,
+    'append_bodies': False,
+    'bodies': bodies,
+    'body': {
+        'num_bodies': 5,
+        'mass_max': 100,
+        'x_max': X_SIZE - 50,
+        'y_max': Y_SIZE - 50,
+        'velocity_max': 0,
+        'trail_length': 40,
+        'random_color': False,
+        'trail_color': 'darkslategray',
+        'color': "medium sea green"
+    }
+}
 
-app = App(X_SIZE, Y_SIZE, bodies)
+app = App(properties)
 app.mainloop()
